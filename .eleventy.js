8fa43a04f2233f7628903e9180ac0a3686b22874
tiltproofRain <srijannr1@gmail.com>
2020-09-28 22:04:19 +0530

    added home page

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,18 @@
+const strftime = require('strftime');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
-  
-  syntaxHighlight.hooks.add('before-sanity-check', function (env) {
-    env.code = env.element.innerText;
-  });
-
+  eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addNunjucksFilter('formatDate', (date) => {
+    return strftime('%B %d, %Y', date);
+  });
 
   return {
     dir: {
       input: '11ty',
-      outputL: '_site',
+      output: 'blog',
     },
     passthroughFileCopy: true,
   };
