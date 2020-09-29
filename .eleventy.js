441ae094819678afbfed3b5b67e4a4aa857be935
tiltproofRain <srijannr1@gmail.com>
2020-09-29 15:15:13 +0530

    added index to home

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('index.html');
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addNunjucksFilter('formatDate', (date) => {
     return strftime('%B %d, %Y', date);
